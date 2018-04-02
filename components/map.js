bd46263990e860f1683ad1e0549b72e448e7768f
Zbeb <zbeb@me.com>
2018-04-02 16:00:06 +0200

    R0002 Added dummies users locations and displayed it on map

--- components/map.js
+++ components/map.js
@@ -6,7 +6,13 @@ Vue.component('reso-map', {
     template: jQuery('#reso-map-template').html(),
     data: function() {
         return {
-            map: undefined
+            map: undefined,
+            userLocs: [
+                        {lat: -33.865, lng: 151.209},
+                        {lat: -35.542, lng: 150.305},
+                        {lat: -32.777, lng: 151.402},
+                        {lat: -34.666, lng: 151.802},
+            ]
         }
     },
     methods: {
@@ -19,8 +25,22 @@ Vue.component('reso-map', {
             console.log('Event recieved, init map');
             self.map = new google.maps.Map(document.getElementById('reso-map'), {
                 center: {lat: -34.397, lng: 150.644},
-                zoom: 8
+                zoom: 6
             });
+
+            self.userLocs.forEach(function(userLoc, index) {
+                var marker = new google.maps.Marker({
+                  position: userLoc,
+                  map: self.map,
+                  title: "User " + index + 1
+                });
+            });
+
         })
+
+        // map.addListener('click', function(e) {
+        //     console.log("coucou");
+        // });
+
     }
 });
